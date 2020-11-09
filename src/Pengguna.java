420368b7af9b19c0ede9d41f88a34cbecd6d197b
Tarangga Arief <tarangga.arief@gmail.com>
2020-11-09 23:55:17 +0700

    next

--- src/Pengguna.java
+++ src/Pengguna.java
@@ -1,6 +1,27 @@
+public class Pengguna extends Manusia {
 
-public class Pengguna {
-	public String PenggunaName;
-	
-	
+    public String penggunaName;
+    
+    public void cari(String keyword ){
+        //
+    }
+    
+
+    public String daftarPelatihan(Pelatihan pelatihan){
+         return pelatihan.linkPelatihan;
+         
+   }
+    
+     public String daftarEvent(Event event){
+          return event.linkEvent;
+         
+   }
+    
+
+    public void isiFormulirEvent(String penggunaName, String email ){
+        this.penggunaName = penggunaName;
+        this.email  = email;
+        
+    }
+    
 }
--- src/Search.java
+++ src/Search.java
@@ -1,4 +1,24 @@
 
 public class Search {
+	public String keyword;
+	public String eventName;
+	public String pelatihanName;
+	public String kolaboratorName;
+	public String kategoriPelatihan;
+	public String kategoriEvent;
 	
+	public void getByKolaborator(String keyword) {
+		
+	}
+	
+	public void getByEventName(String keyword) {
+		
+	}
+	public void getByPelatihanName(String keyword) {
+		
+	}
+	
+	public void getByKategori(String keyword) {
+		
+	}
 }
